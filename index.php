9ddcc7111bb1ab186ea0eeb09f63ffc3af226052
deltas92 <info@kjell-heinsen.de>
2020-04-20 12:27:23 +0200

    v0.0.0.3

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('log_errors', 'On');
+ini_set('error_log', 'php-errors.log');
+ob_start();
+
+
+$root = dirname(__FILE__);
+$root = $root . '/';
+DEFINE('DOCROOT', $root);
+
+function autoloadsystem($class)
+{
+
+    $filename = str_replace('\\', '/', $class . '.php');
+    $filename = DOCROOT . strtolower($filename);
+    if (file_exists($filename)) {
+        require_once $filename;
+    }
+
+}
+
+spl_autoload_register("autoloadsystem");
+
+?>
---
+++ lib/database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database extends mysqli{
+ 
+    private $link = null;
+
+    
+    
+
+        public function __construct()
+    {
+ 
+        try {
+            $this->link = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME );
+            $this->link->set_charset( "utf8" );
+        } catch ( Exception $e ) {
+            die( 'Es kann keine Verbindung zur Datenbank hergestellt werden.' );
+        }
+    }
+    public function __destruct()
+    {
+        if( $this->link)
+        {
+            $this->disconnect();
+        }
+    }
+ 
+    
+
+    public function disconnect()
+    {
+        $this->link->close();
+    }
+}
+    
+
