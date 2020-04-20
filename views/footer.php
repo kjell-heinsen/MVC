8d9a228e8e85866f36bcf7408adba13f2af5dc66
deltas92 <info@kjell-heinsen.de>
2020-04-20 16:58:12 +0200

    v0.0.0.12

--- views/footer.php
+++ views/footer.php
@@ -5,6 +5,6 @@
 
 </footer>
 <!-- Platz für JS -->
-<script src="https://ofinder.de/plugins/jquery/jq341.js"></script>
-<script src="https://ofinder.de/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/plugins/jquery/jq341.js"></script>
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 </html>
