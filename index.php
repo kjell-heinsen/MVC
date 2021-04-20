753ff9ed0fab43c2dfabdd4022545af7455ae237
kjell-heinsen <info@kjell-heinsen.de>
2021-04-20 14:54:14 +0200

    v0.0.0.21

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 $HasConfig = 0;
 $myurl = '';
+
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 ini_set('log_errors', 'On');
 ini_set('error_log', 'php-errors.log');
+
+// Definierung wichtiger Software-Parameter
+$softwareURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".$_SERVER['HTTP_HOST']."/";
+define("DIR",$softwareURL);
 ob_start();
+
 
 
 $root = dirname(__FILE__);
@@ -25,20 +31,30 @@ function autoloadsystem($class)
 }
 spl_autoload_register("autoloadsystem");
 use  core\main;
-
+$session = new main\Session();
+$session->init();
 $app = new main\App();
+$app->init();
+/*
+$redirect = new main\URL();
 $myurl = $app->_url;
 if (file_exists(CONFIGFILE)) {
     require_once CONFIGFILE;
     $HasConfig = 1;
 } else {
-   $app->GetInstaller();
+
+ define("INSTALLER",'installer/');
+   $redirect->redirect(INSTALLER,303);
+    echo $this->_url[0];
+    if($this->_url[0]===INSTALLER) {
+        $app->GetInstaller();
+    }
 }
 
 if($HasConfig === 1)
 {
 $app->init();
-}
+} */
 
 
 
